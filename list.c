3d2813e9045e54850734813e01d03b97f951eac4
xuzhuyun <xuzhuyun@umich.edu>
2015-09-15 01:04:37 -0400

    Update list.c

--- list.c
+++ list.c
@@ -10,7 +10,24 @@
 list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
-//
+	list_t* ptr = head;
+	while (ptr->next != NULL){
+		if (ptr->next->index < new_element->index) {
+			ptr = ptr->next;
+		}else {
+			new_element->next = ptr->next;
+			ptr->next = new_element;
+			break;
+		}
+	}
+	if (ptr->next == NULL){
+		if (ptr == head) {
+			new_element->next = head;
+			head = new_element;	
+		}else{
+			ptr->next = new_element;
+		}
+	}
 	return head;
 }
 
